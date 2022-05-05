616f55f0b3320138dc840ee33bc65f4e33962310
Raghu B M <raghubm2222@gmail.com>
2022-05-05 16:13:53 +0530

    Final

--- src/test/java/com/goutham/App.java
+++ src/test/java/com/goutham/App.java
@@ -3,8 +3,12 @@ package com.goutham;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,41 +17,41 @@ public class App {
     static String baseURL = "https://www.urbanladder.com";
     static WebDriver driver;
 
+    @BeforeTest
+    void initilize() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(baseURL);
+    }
+
     @Test
-    void startTests() {
-        List<TestData> testDataList = DatabaseOperations.getTestList();
-        for (TestData testData : testDataList)
-            try {
-                startTest(testData);
-                System.out.println("Test " + testData.getTestId() + " completed Successfully");
-            } catch (InterruptedException e) {
-                System.out.println("Exception occured while executing test: " + testData.getTestId());
-            }
+    void test() throws Exception {
+        driver.manage().window().maximize();
+        WebElement study = driver.findElement(By.xpath("//*[@id='topnav_wrapper']/ul/li[6]/span"));
+        Actions action = new Actions(driver);
+        action.moveToElement(study).perform();
+        Thread.sleep(3000);
+        WebElement bookshelves = driver
+                .findElement(By.xpath("//*[@id='topnav_wrapper']/ul/li[6]/div/div/ul/li[3]/ul/li[1]/a/span"));
+        action.moveToElement(bookshelves).perform();
+        bookshelves.click();
+        Thread.sleep(5000);
+        action.sendKeys(Keys.ESCAPE).perform();
+        Thread.sleep(5000);
+        action.moveToElement(driver.findElement(By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[1]/div[1]")))
+                .perform();
+        Thread.sleep(1000);
+        WebElement dragElement = driver.findElement(By.xpath(
+                "//*[@id='filters-form']/div[1]/div/div/ul/li[1]/div[2]/div/div/ul/li[1]/div/div[2]/div[2]/div/div[2]/div"));
+        action.dragAndDropBy(dragElement, -202, 0).perform();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id='filters_availability_In_Stock_Only']")).click();
+        Thread.sleep(1000);
+        action.moveToElement(driver.findElement(By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[2]"))).perform();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id='filters_storage_type_Open']")).click();
+        Thread.sleep(3000);
+        List<WebElement> producList = driver.findElements(By.className("product"));
+        System.out.println(producList.size());
     }
-
-    static void startTest(TestData testData) throws InterruptedException {
-        try {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.get(baseURL);
-            Thread.sleep(1000);
-            driver.manage().window().maximize();
-            driver.findElement(By.xpath("//*[@id='header']/section/div/ul[2]/li[3]/a")).click();
-            driver.findElement(By.xpath("//*[@id='app-container']/div/header/section/div/ul[2]/li[3]/a")).click();
-            driver.findElement(By.xpath("/html/body/div[1]/div/main/section/section[1]/ul/li[3]")).click();
-            driver.findElement(By.xpath("//*[@id='ip_2251506436']")).sendKeys("1000");
-            driver.findElement(By.xpath("//*[@id='app-container']/div/main/section/section[2]/div/section[2]/button")).click();
-            driver.findElement(By.name("recipient_name")).sendKeys(testData.getRecipientName());
-            driver.findElement(By.name("recipient_email")).sendKeys(testData.getRecipientEmail());
-            driver.findElement(By.name("customer_name")).sendKeys(testData.getSenderName());
-            driver.findElement(By.name("customer_email")).sendKeys(testData.getSenderEmail());
-            driver.findElement(By.name("customer_mobile_number")).sendKeys(testData.getSenderPhoneNumber());
-            driver.findElement(By.xpath("//*[@id='app-container']/div/main/section/section[3]/form/button")).click();
-            Thread.sleep(1000);
-        } finally {
-            driver.quit();
-        }
-
-    }
-
 }
